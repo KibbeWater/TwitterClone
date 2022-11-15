b4b7ccff3d2cc07ceae681d1d91e7808d30ee354
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2022-11-15 15:34:13 +0100

    Begun work on profile editing

---
+++ components/EditProfileModal.js
@@ -0,0 +1,15 @@
+class EditProfile extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			bio: '',
+			avatar: '',
+			banner: '',
+			loading: false,
+			error: '',
+		};
+	}
+
+	render() {}
+}
