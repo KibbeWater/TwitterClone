4cf4dec3e5456d0ec72fbd70e6d31fff037f7329
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2022-11-23 16:02:03 +0100

    Started work on Post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,37 +1,49 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRepeat } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 
 type PostObject = {
-    id: number;
-    userId: number;
-    content: string;
-    ref: number;
-    parent: number;
-    data: number;
-}
+	id: number;
+	userId: number;
+	content: string;
+	ref: number;
+	parent: number;
+	date: number;
+};
 
 type Props = {
-    post: PostObject;
-}
+	post: PostObject;
+};
 
-export default function Post({post}: Props) {
-    <div className="post">
-        <Image src="{$post->author->avatar}" alt="{$post->author->username}'s avatar" class="post__author_avatar">
-        <div class="post__content">
-            <div class="post__header">
-                <a class="post__author_username" href="/@{$post->author->tag}">{$post->author->username}</a>
-                <a class="post__author_tag" href="/@{$post->author->tag}">@{$post->author->tag} ·</a>
-                <span class="post__timestamp">{$post->timestamp}</span>
-            </div>
-            {$post->content}
-            {$refHtml}
-            <div class="post__footer">
-            <button id="btnRetwat" class="post__footer_button">
-                <i class="fa-solid fa-repeat fa-xl"></i>
-            </button>
-            <button id="btnLike" class="post__footer_button__like" data="" . ($isLiked ? 'liked' : 'unliked') . "\">
-        <i class=\"" . ($isLiked ? 'fas' : 'far') . " fa-heart fa-xl\"></i>
-    </button>
-        </div>
-        </div>
-    </div>
+export default function Post({ post }: Props) {
+	<div className={'p-3 mb-px w-full bg-transparent transition-all cursor-pointer border-b-[1px] border-gray-700 flex hover:bg-black/5'}>
+		<Image src='{$post->author->avatar}' alt="{$post->author->username}'s avatar" className='post__author_avatar' />
+		<div className='post__content'>
+			<div className='post__header'>
+				<a className='post__author_username' href='/@{$post->author->tag}'>
+					{'post->author->username'}
+				</a>
+				<a className='post__author_tag' href='/@{$post->author->tag}'>
+					{'@post->author->tag ·'}
+				</a>
+				<span className='post__timestamp'>{new Date(post.date).toDateString()}</span>
+			</div>
+			<p>{post.content}</p>
+			{post.ref !== -1 ? (
+				<></>
+			) : (
+				<div className='post__reference'>
+					<Post />
+				</div>
+			)}
+			<div className='post__footer'>
+				<button id='btnRetwat' className='post__footer_button'>
+					<FontAwesomeIcon icon={faRepeat} size={'xl'} />
+				</button>
+				<button id='btnLike' className='post__footer_button__like'>
+					<i class='fa-heart fa-xl\'></i>
+				</button>
+			</div>
+		</div>
+	</div>;
 }
