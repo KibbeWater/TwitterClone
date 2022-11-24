c2c0bbab45d043c58414396ceb64197d86a334b8
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2022-11-24 18:37:11 +0100

    presign stuff

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
-}
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { s3Client } from '../../libs/storage';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	getSignedUrl(
+		s3Client,
+		new GetObjectCommand({
+			Bucket: 'kibbewater-twatter',
+			Key: '/avatars/7fe0e69f09798fc7c774d21b21641490.jpg',
+		}),
+		{ expiresIn: 3600 }
+	)
+		.then((url) => {
+			res.status(200).json({ url });
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).json({ err });
+		});
 }
