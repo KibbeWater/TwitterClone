e36f1f1a0f945dac9a53571299910f193e7a44f1
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2022-11-25 08:23:57 +0100

    Added basic Schemas

---
+++ schemas/IPost.ts
@@ -0,0 +1,21 @@
+import { model, Schema, Types } from 'mongoose';
+
+interface IPost {
+	user?: Types.ObjectId;
+	content: string;
+	quote?: Types.ObjectId;
+	comments: [Types.ObjectId];
+	date: number;
+}
+
+const postSchema = new Schema<IPost>({
+	user: { type: Types.ObjectId, ref: 'User' },
+	content: { type: String, required: true },
+	quote: { type: Types.ObjectId, ref: 'Post' },
+	comments: [{ type: Types.ObjectId, ref: 'Post' }],
+	date: { type: Number, required: true },
+});
+
+const PostModel = model<IPost>('Post', postSchema);
+
+export default PostModel;
---
+++ schemas/IUser.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+interface IUser {
+	tag: string;
+	username: string;
+	password: string;
+
+	avatar?: string;
+	banner?: string;
+	bio: string;
+
+	group: number;
+}
+
+const userSchema = new Schema<IUser>({
+	tag: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+
+	avatar: { type: String, default: null },
+	banner: { type: String, default: null },
+	bio: { type: String, default: '' },
+
+	group: { type: Number, default: 0 },
+});
+
+const UserModel = model<IUser>('User', userSchema);
+
+export default UserModel;
