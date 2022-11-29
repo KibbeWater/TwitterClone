b7e987f0c3c96ee5622001264b238b21d925cf49
Snow <35224538+KibbeWater@users.noreply.github.com>
2022-11-29 23:15:05 +0100

    Added some OG meta

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,11 @@ export default function Head() {
 			<title>Twatter</title>
 			<meta content='width=device-width, initial-scale=1' name='viewport' />
 			<meta name='description' content='Not Twitter' />
+			<meta property='og:title' content='Twatter' />
+			<meta property='og:url' content='https://twatter-kibbewater.vercel.app/home' />
+			<meta property='og:description' content='Definitely not a Twitter clone' />
+			<meta property='og:type' content='website' />
+			<meta property='og:image' content='/assets/favicons/android-chrome-512x512.png' />
 			<link rel='icon' href='/favicon.ico' />
 		</>
 	);
