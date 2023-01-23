c88878155fffe2f869266b947811c566aa990074
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2023-01-23 13:55:38 +0100

    Begun work on a video multipart uploader

--- libs/storage.ts
+++ libs/storage.ts
@@ -1,5 +1,6 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { GetObjectCommand, PutObjectCommand, S3Client, UploadPartCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import axios from 'axios';
 import { GenerateStorageKey } from './utils';
 
 export const S3_REGION = process.env.S3_REGION || 'us-east-1';
@@ -59,4 +60,82 @@ export function uploadImage(dataUri: string): Promise<string | null> {
 				reject(err);
 			});
 	});
+}
+
+export function videoChunkUpload(fileName: string, partId: number, uploadId: string) {
+	return new Promise<string | null>((resolve, reject) => {
+		const bucket = S3_BUCKET;
+		const key = `video-raw/${fileName}`;
+
+		getSignedUrl(
+			s3Client,
+			new UploadPartCommand({
+				Bucket: bucket,
+				Key: key,
+				PartNumber: partId,
+				UploadId: uploadId,
+			})
+		).then((url) => {
+			resolve(url);
+		});
+	});
+}
+
+// 50 MB
+const CHUNK_SIZE = 1024 * 1024 * 50;
+
+type UploadURL = { url: string; partId: number; key: string; uploadId: string };
+
+export class MultipartUploader {
+	private data: ArrayBuffer;
+	private chunks: ArrayBuffer[] = [];
+
+	private uploadURLs: UploadURL[] = [];
+	private videoId: string = '';
+	private uploadKey: string = '';
+
+	constructor(file: string) {
+		// Parse file as a data URI, retreive the base64 encoded data and decode it into a raw buffer. Retreive also the content type, length and extension.
+		const buffer = Buffer.from(file.split(',')[1], 'base64');
+		const contentType = file.split(';')[0].split(':')[1];
+		const contentLength = buffer.length;
+		const ext = file.split(';')[0].split('/')[1];
+
+		// Convert the raw buffer into an ArrayBuffer.
+		this.data = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+	}
+
+	public async upload() {
+		// Generate chunks of the file.
+		this.chunks = await this.generateChunks(CHUNK_SIZE);
+
+		await this.retreiveUploadInformation();
+	}
+
+	private async generateChunks(chunkSize: number) {
+		const chunks: ArrayBuffer[] = [];
+
+		for (let i = 0; i < this.data.byteLength; i += chunkSize) {
+			chunks.push(this.data.slice(i, i + chunkSize));
+		}
+
+		return chunks;
+	}
+
+	private retreiveUploadInformation() {
+		return new Promise((resolve, reject) => {
+			axios
+				.post<{ success: boolean; videoId: string; urls: UploadURL[] }>('/api/video/upload', {
+					chunks: this.chunks.length,
+				})
+				.then((res) => {
+					this.uploadURLs = res.data.urls;
+					this.videoId = res.data.videoId;
+				});
+		});
+	}
+
+	private uploadPart(retry: number = 0) {
+		return new Promise((resolve, reject) => {});
+	}
 }
---
+++ pages/api/video/upload.ts
@@ -0,0 +1,63 @@
+import { CreateMultipartUploadCommand } from '@aws-sdk/client-s3';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { s3Client, videoChunkUpload } from '../../../libs/storage';
+
+function initializeUpload(req: NextApiRequest, res: NextApiResponse) {
+	return new Promise((resolve) => {
+		const { chunks }: { chunks: number } = req.body;
+
+		// Generate random 16 char id for the video.
+		const id = [...Array(16)]
+			.map(() => (~~(Math.random() * 36)).toString(36))
+			.join('')
+			.toLowerCase();
+
+		s3Client
+			.send(
+				new CreateMultipartUploadCommand({
+					Bucket: process.env.S3_BUCKET as string,
+					Key: `video_raw/${id}.mp4`,
+					ContentType: 'video/mp4',
+					Metadata: {
+						chunks: chunks.toString(),
+					},
+				})
+			)
+			.then((data) => {
+				resolve(
+					res.status(200).json({
+						success: true,
+						videoId: id,
+						urls: [...Array(chunks)].map((_, i) => {
+							return new Promise<{ url: string; partId: number; key: string; uploadId: string }>((resolve) => {
+								videoChunkUpload(`${id}.mp4`, i + 1, data.UploadId as string).then((url) =>
+									resolve({
+										url: url as string,
+										partId: i + 1,
+										key: `video_raw/${id}.mp4`,
+										uploadId: data.UploadId as string,
+									})
+								);
+							});
+						}),
+					})
+				);
+			})
+			.catch(() => {
+				resolve(res.status(500).json({ success: false, error: 'Internal server error' }));
+			});
+	});
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	return new Promise((resolve) => {
+		switch (req.method) {
+			case 'POST':
+				initializeUpload(req, res).then(resolve);
+				break;
+			default:
+				resolve(res.status(405).json({ message: 'Method not allowed' }));
+				break;
+		}
+	});
+}
