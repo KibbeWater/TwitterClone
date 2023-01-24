d2da0f953675b14340ce115b5a1734a5046aa77e
Snow <35224538+KibbeWater@users.noreply.github.com>
2023-01-24 16:13:20 +0100

    Lazy loading framer-motion

--- components/Modals/RegisterModal.tsx
+++ components/Modals/RegisterModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useContext, useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { m as motion } from 'framer-motion';
 import axios from 'redaxios';
 
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
