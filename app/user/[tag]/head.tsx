fe5763553d70cf38c1a3894d01f1e6195e7a77b3
Snow <35224538+KibbeWater@users.noreply.github.com>
2023-02-09 21:13:31 +0100

    Fixed headers

--- app/user/[tag]/head.tsx
+++ app/user/[tag]/head.tsx
@@ -1,16 +1,24 @@
-import Post from '../../../schemas/IPost';
-import { IUser } from '../../../types/IUser';
+import User from '../../../schemas/IUser';
 
 type Props = {
 	params: {
-		id: string;
+		tag: string;
 	};
 };
 
 export default async function Head({ params }: Props) {
-	const post = await Post.findOne({ _id: params.id }).populate('user');
+	const tag = params.tag.replace('%20', '');
 
-	const user = post?.user as IUser | undefined;
+	const user = await User.findOne({ username: tag });
+
+	if (!user)
+		return (
+			<>
+				<title>Twatter</title>
+				<meta content='width=device-width, initial-scale=1' name='viewport' />
+				<meta name='description' content='Not Twitter' />
+			</>
+		);
 
 	return (
 		<>
@@ -19,21 +27,21 @@ export default async function Head({ params }: Props) {
 			<meta name='description' content='Not Twitter' />
 
 			<meta property='og:site_name' content='Twatter' />
-			<meta property='og:url' content={'https://twatter-kibbewater.vercel.app/post/' + params.id} />
+			<meta property='og:url' content={'https://twatter-kibbewater.vercel.app/user/@' + tag} />
 			<meta property='og:image' content='https://twatter-kibbewater.vercel.app/assets/favicons/icon-512x512.png' />
 
-			{/* Create OG meta tags to describe the post, treat this as an article */}
-			<meta property='og:description' content={post?.content} />
+			{/* Create OG meta tags to describe the user profile */}
+			<meta property='og:description' content={user?.bio} />
 			<meta property='og:title' content={`${user?.username} on Twatter`} />
 			<meta property='og:image' content={user?.avatar} />
 			<meta property='og:image:alt' content={user?.username} />
-			<meta property='og:type' content='article' />
+			<meta property='og:type' content='profile' />
 
 			<meta name='twitter:card' content='summary_large_image' />
 			<meta name='twitter:site' content='@kibbewater' />
 			<meta name='twitter:creator' content='@kibbewater' />
 			<meta name='twitter:title' content='Twatter' />
-			<meta name='twitter:description' content={post?.content} />
+			<meta name='twitter:description' content={user?.bio} />
 			<meta name='twitter:image' content={user?.avatar} />
 
 			<meta name='theme-color' content='#f01d1d' />
