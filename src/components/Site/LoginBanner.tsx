4c7c95236e97cd2bac37247bbf342809b5d94572
Snow <35224538+KibbeWater@users.noreply.github.com>
2023-10-09 20:04:38 +0200

    Fixed LoginBanner for mobile users

--- src/components/Site/LoginBanner.tsx
+++ src/components/Site/LoginBanner.tsx
@@ -2,13 +2,13 @@ import { signIn } from "next-auth/react";
 
 export default function LoginBanner() {
     return (
-        <div className="w-full h-20 bg-accent-primary-500 grid grid-cols-3 text-white">
-            <div />
+        <div className="w-full h-14 md:h-20 bg-accent-primary-500 flex justify-between lg:px-0 md:px-8 px-3 lg:grid lg:grid-cols-3 text-white">
+            <div className="hidden lg:block" />
             <div className="flex flex-col justify-center">
-                <h2 className="text-2xl font-bold leading-snug">
+                <h2 className="text-sm md:text-2xl font-bold leading-snug">
                     Don’t miss what’s happening
                 </h2>
-                <p className="leading-snug">
+                <p className="leading-snug text-xs md:text-base">
                     People on Twatter are the first to know.
                 </p>
             </div>
@@ -17,7 +17,7 @@ export default function LoginBanner() {
                     onClick={() => {
                         signIn().catch((e) => console.error(e));
                     }}
-                    className="border-white hover:bg-white/10 transition-colors duration-500 border-[1px] px-4 py-1 font-semibold rounded-full"
+                    className="border-white hover:bg-white/10 transition-colors duration-500 border-[1px] px-2 lg:px-4 py-1 font-semibold rounded-full text-sm lg:text-base"
                 >
                     Log in
                 </button>
@@ -25,7 +25,7 @@ export default function LoginBanner() {
                     onClick={() => {
                         signIn().catch((e) => console.error(e));
                     }}
-                    className="bg-white hover:bg-gray-200 transition-colors duration-500 text-black px-4 py-1 font-semibold rounded-full"
+                    className="bg-white hover:bg-gray-200 transition-colors duration-500 text-black px-2 lg:px-4 py-1 font-semibold rounded-full text-sm lg:text-base"
                 >
                     Sign up
                 </button>
