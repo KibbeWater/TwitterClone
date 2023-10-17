65f8d55f448fc98c55eb2d3ef03bdbd0d3c7ac20
Snow <35224538+KibbeWater@users.noreply.github.com>
2023-10-17 14:53:14 +0200

    Added placeholder tabs

--- src/components/Site/Layouts/SettingsLayout.tsx
+++ src/components/Site/Layouts/SettingsLayout.tsx
@@ -1,18 +1,28 @@
 import Head from "next/head";
 import Navbar from "../Navbar";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { useMemo } from "react";
 
 export default function SettingsLayout({
     children,
 }: {
     children?: React.ReactNode;
 }) {
+    const tabs = useMemo<{ name: string }[]>(
+        () => [
+            { name: "Your account" },
+            { name: "Security and account access" },
+            { name: "Privacy and safety" },
+        ],
+        [],
+    );
+
     return (
         <>
             <Head>
                 <title>Twatter - Settings</title>
             </Head>
-            <div className="parent w-screen h-screen flex relative">
+            <div className="parent w-screen h-screen flex relative bg-white dark:bg-black">
                 <Navbar />
 
                 <div className="w-1/5 border-r-[1px] border-gray-200 dark:border-gray-700">
@@ -22,12 +32,17 @@ export default function SettingsLayout({
                         </h1>
                     </div>
                     <nav className="flex flex-col">
-                        <div className="flex justify-between items-center px-4 py-2 hover:bg-neutral-700">
-                            <p className="text-lg">Your account</p>
-                            <div className="h-8 w-8">
-                                <ChevronRightIcon />
-                            </div>
-                        </div>
+                        {tabs.map((tab, idx) => (
+                            <button
+                                key={`${tab.name}-${idx}`}
+                                className="flex justify-between items-center pl-4 pr-3 py-2 transition-colors bg-transparent duration-300 dark:hover:bg-neutral-700 hover:bg-neutral-200"
+                            >
+                                <p>{tab.name}</p>
+                                <div className="h-6 w-6">
+                                    <ChevronRightIcon className="text-neutral-500" />
+                                </div>
+                            </button>
+                        ))}
                     </nav>
                 </div>
                 <main></main>
